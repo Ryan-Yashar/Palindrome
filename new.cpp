48fd882cf6036a63f1281d7fedd4490053b02b37
U-BSD\482467 <482467@SHS-JYBF6X3.beaverton.k12.or.us>
2026-09-09 09:46:12 -0700

    file strips whitespace now

---
+++ new.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+char* stripNonAlphanumeric(char *readin) {
+  char *readout = new char[81];
+  int readoutptr = 0;
+  for (int i = 0; readin[i] != '\0'; i++) {
+    if ((readin[i] > 47 && readin[i] < 58) || (readin[i] > 64 && readin[i] < 91) || (readin[i] > 96 && readin[i] < 123)) {
+      readout[readoutptr] = readin[i];
+      readoutptr += 1;
+    }
+  }
+  readout[readoutptr] = '\0';
+  return readout;
+}
+
+int main() {
+  char *readin = new char[81];
+  char *readinptr = readin;
+  cin.get(readin, 81);
+  readin = stripNonAlphanumeric(readin);
+  delete[] readinptr;
+  cout << readin;
+  return 0;
+}
